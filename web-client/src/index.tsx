6ccd426dfcb1ee9fde3cb73fa7f18e249d79c6d4
blinikar <blinov.egor2013@mail.ru>
2022-06-03 18:11:22 +0300

    pages in structure

--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { Layout } from "./components/Layout";
-import { LoginPage } from "./components/LoginPage";
-import DevTimetable from "./Timetable.dev";
-import {ScheduleEditor} from "./components/ScheduleEditor";
+import { Layout } from "./pages/Layout";
+import { LoginPage } from "./pages/LoginPage";
+import DevTimetable from "./pages/Timetable.dev";
+import {ScheduleEditor} from "./pages/ScheduleEditor";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- web-client/src/pages/ScheduleEditor/ScheduleEditor.tsx
+++ web-client/src/pages/ScheduleEditor/ScheduleEditor.tsx
@@ -3,6 +3,6 @@ import styles from "./ScheduleEditor.module.scss";
 
 export const ScheduleEditor:React.FunctionComponent = () => {
     return <div className={styles["ScheduleEditor"]}>
-        <div className={styles["toolbar"]}></div>
+        <div className={styles["toolbar"]}> </div>
     </div>;
 }
