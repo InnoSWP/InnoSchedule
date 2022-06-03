cc41162f72eb3c8f96c56083662534fd6c411b41
blinikar <blinov.egor2013@mail.ru>
2022-06-03 18:37:21 +0300

    colors

--- web-client/src/pages/ScheduleEditor/ScheduleEditor.tsx
+++ web-client/src/pages/ScheduleEditor/ScheduleEditor.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./ScheduleEditor.module.scss";
 
+import { Button } from "evergreen-ui";
+
 export const ScheduleEditor:React.FunctionComponent = () => {
     return <div className={styles["ScheduleEditor"]}>
-        <div className={styles["toolbar"]}> </div>
+        <div className={styles["toolbar"]}>
+            <Button>Core courses</Button>
+            <Button>Electives</Button>
+        </div>
     </div>;
 }
