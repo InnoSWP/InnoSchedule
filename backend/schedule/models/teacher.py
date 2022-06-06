71a2594d9df01b2b0ea54f8671d65e2ec550fd6b
Egor <edkuz11@yandex.ru>
2022-06-06 20:53:20 +0300

    Teacher model update

--- backend/schedule/models/teacher.py
+++ backend/schedule/models/teacher.py
@@ -2,7 +2,6 @@ import uuid
 from django.db import models
 
 
-# Create your models here.
 class TeacherModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=256)
