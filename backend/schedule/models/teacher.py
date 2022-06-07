1c29ed873f2c37c0cb098d132cc842cf67475587
Egor <edkuz11@yandex.ru>
2022-06-07 13:59:18 +0300

    Teacher model unique uuid fix

--- backend/schedule/models/teacher.py
+++ backend/schedule/models/teacher.py
@@ -3,5 +3,5 @@ from django.db import models
 
 
 class TeacherModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=256)
