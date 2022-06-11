534d2bccd87c3572418c2b51f55156cafca49249
Egor <edkuz11@yandex.ru>
2022-06-11 17:42:12 +0300

    Views changed to viewset and added to URLs

--- backend/schedule/views/teacher.py
+++ backend/schedule/views/teacher.py
@@ -1,14 +1,16 @@
-from rest_framework import generics
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from rest_framework_bulk import mixins as bulk_mixins
 
 from ..models import TeacherModel
 from ..serializers import TeacherSerializer
 
 
-class TeachersListView(generics.ListCreateAPIView):
-    queryset = TeacherModel.objects.all()
-    serializer_class = TeacherSerializer
-
-
-class TeacherView(generics.RetrieveUpdateDestroyAPIView):
+class TeacherViewSet(mixins.ListModelMixin,
+                     mixins.RetrieveModelMixin,
+                     bulk_mixins.BulkCreateModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     GenericViewSet):
     queryset = TeacherModel.objects.all()
     serializer_class = TeacherSerializer
