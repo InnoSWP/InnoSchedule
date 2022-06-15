b094f9c733fbd1449ec80241aa036ed81132cced
Egor <edkuz11@yandex.ru>
2022-06-15 14:38:53 +0300

    Room viewset added to urls

--- backend/schedule/urls.py
+++ backend/schedule/urls.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import TeacherViewSet
+from .views.teacher import TeacherViewSet
+from .views.room import RoomViewSet
 
 router = DefaultRouter()
 router.register("teachers", TeacherViewSet, basename="teacher")
+router.register("rooms", RoomViewSet, basename="room")
 
 urlpatterns = router.urls
