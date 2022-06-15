a06b191bbf08e8d30bbb18207c822fcd19c19a1f
Egor <edkuz11@yandex.ru>
2022-06-15 14:24:01 +0300

    Added permissions to Teacher view

--- backend/schedule/views/teacher.py
+++ backend/schedule/views/teacher.py
@@ -1,7 +1,9 @@
 from rest_framework import mixins
+from rest_framework import permissions
 from rest_framework.viewsets import GenericViewSet
 from rest_framework_bulk import mixins as bulk_mixins
 
+from ._mixins import PermissionsPolicyMixin
 from ..models import TeacherModel
 from ..serializers import TeacherSerializer
 
@@ -11,6 +13,8 @@ class TeacherViewSet(mixins.ListModelMixin,
                      bulk_mixins.BulkCreateModelMixin,
                      mixins.UpdateModelMixin,
                      mixins.DestroyModelMixin,
+                     PermissionsPolicyMixin,
                      GenericViewSet):
     queryset = TeacherModel.objects.all()
     serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAdminUser]
