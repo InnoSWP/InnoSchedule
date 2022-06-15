c56e4b87ac73c8794a1a06848d5a5e6d3735f55a
Egor <edkuz11@yandex.ru>
2022-06-15 14:23:37 +0300

    String representation of Teacher model

--- backend/schedule/models/teacher.py
+++ backend/schedule/models/teacher.py
@@ -5,3 +5,6 @@ from django.db import models
 class TeacherModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
