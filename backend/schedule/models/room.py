cea748452673effe1d88d90c0f7419583bde96dc
Egor <edkuz11@yandex.ru>
2022-06-15 14:51:02 +0300

    Adding unique to Room and Teacher model name

--- backend/schedule/models/room.py
+++ backend/schedule/models/room.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class RoomModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, unique=True)
 
     def __str__(self):
         return self.name
