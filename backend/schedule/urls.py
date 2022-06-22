a9771565cb12bb2183f261b1fe47b752878978aa
Egor <edkuz11@yandex.ru>
2022-06-22 17:30:28 +0300

    Schedule and Course added to urls.py with drf-nested-routers

--- backend/schedule/urls.py
+++ backend/schedule/urls.py
@@ -1,12 +1,20 @@
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import DefaultRouter, NestedSimpleRouter
 
-from .views.teacher import TeacherViewSet
-from .views.room import RoomViewSet
 from .views.activity import ActivityViewSet
+from .views.course import CourseViewSet
+from .views.room import RoomViewSet
+from .views.schedule import ScheduleViewSet
+from .views.teacher import TeacherViewSet
 
 router = DefaultRouter()
 router.register("teachers", TeacherViewSet, basename="teacher")
 router.register("rooms", RoomViewSet, basename="room")
 router.register("activities", ActivityViewSet, basename="activity")
+router.register("schedules", ScheduleViewSet, basename="schedule")
 
-urlpatterns = router.urls
+schedules_router = NestedSimpleRouter(router, "schedules", lookup="schedule")
+schedules_router.register("courses", CourseViewSet, basename="schedule-course")
+
+urlpatterns = []
+urlpatterns += router.urls
+urlpatterns += schedules_router.urls
