da6e54168df26d1c751dbd579fe4513ec982991d
Egor <edkuz11@yandex.ru>
2022-06-22 17:09:22 +0300

    CORS fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
