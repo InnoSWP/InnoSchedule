cf635039e92bd1c3d956cd62abb69aee0b639abb
Egor <edkuz11@yandex.ru>
2022-06-22 17:01:47 +0300

    Schedule model

---
+++ backend/schedule/models/schedule.py
@@ -0,0 +1,11 @@
+import uuid
+
+from django.db import models
+
+
+class ScheduleModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
